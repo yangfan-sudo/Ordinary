8a09eeafa7fb7458ac8ab8ea575be07b73591053
yangfan <seanyang@lilithgames.com>
2020-03-21 12:50:05 +0800

    上传基础文件

---
+++ Source/Ordinary/Ordinary.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Ordinary.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ordinary, "Ordinary" );
+
+DEFINE_LOG_CATEGORY(LogOrdinary)
+ 
